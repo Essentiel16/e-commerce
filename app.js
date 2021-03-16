589c64a46e2603a9e004d657f28ed61a2cb0e78e
Success Ologunsua <successologunsua@gmail.com>
2021-03-16 13:50:51 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const logger = require('morgan');
 const { userRouter, productRouter } = require('./routes');
 
 const app = express();
+require('dotenv').config();
 
 app.use(express.json());
 app.use(logger('dev'));
@@ -11,6 +12,7 @@ app.get('/', (req, res) => res.json({ welcome: 'hello' }));
 app.use(userRouter);
 app.use(productRouter);
 
-app.listen(3000, () => {
-  console.log(`Example app listening at http://localhost:${3000}`);
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
